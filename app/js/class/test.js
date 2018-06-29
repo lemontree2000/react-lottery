2e9d089176c7d42865535b55b67169d5194d0428
Edward <gonelove.guo@gmail.com>
2018-06-29 23:58:21 +0800

    test Promise api

--- app/js/class/test.js
+++ app/js/class/test.js
@@ -1,9 +1,38 @@
+import { log } from "util";
+
 {
-    let test = function(...arg){
+    let test = function (...arg) {
         console.log(arguments);
         console.log(arguments[1]);
         console.log(arguments[0]);
     }
 
-    test(1,2)
+    test(1, 2)
 }
+
+{
+    const ajax = function () {
+        return new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                const num = Math.random();
+                if (0.6 > 0.5) {
+                    resolve(num)
+                } else {
+                    reject(num)
+                }
+            }, 1000);
+        });
+    }
+ 
+    // ajax().then(function(res) {
+    //     console.log('resolve', res)
+    // }).catch(function(err){
+    //     console.log('err', err);
+    // });
+    Promise.all([ajax(),ajax(),ajax(),ajax()]).then(function(ress) {
+        console.log(ress);
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
