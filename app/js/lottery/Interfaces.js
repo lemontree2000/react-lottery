7afad4c58b93bc8e133b159ef42cc0b4cfd2b8aa
Edward <gonelove.guo@gmail.com>
2018-07-01 23:05:21 +0800

    finnish lottery

--- app/js/lottery/Interfaces.js
+++ app/js/lottery/Interfaces.js
@@ -1,77 +1,79 @@
 import $ from 'jquery';
-import { rejects } from 'assert';
 
-class Interfaces {
-    /**
-     * 获取遗漏数据
-     * @param {string} issue 当前的期号
-     * 
-     */
-    getOmit(issue) {
-        let self = this;
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/get/omit',
-                data: {
-                    issue
-                },
-                dataType: 'json',
-                success: function (res) {
-                    self.setOmit(res.data);
-                    resolve.call(self, res);
-                },
-                error: function (err) {
-                    reject.call(err);
-                }
-            })
-        })
-    }
+class Interfaces{
 
-    /**
-     * 获取开奖号码
-     * @param {string} issue 期号
-     */
-    getOpenCode(issue) {
-        let self = this;
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/get/opencode',
-                data: {
-                    issue
-                },
-                success: function (res) {
-                    self.setOpenCode(res.data);
-                    resolve.call(self, res);
-                },
-                error: function (err) {
-                    reject.call(err);
-                }
-            })
-        })
-    }
+  /**
+   * [getOmit 获取遗漏数据]
+   * @param  {string} issue [当前期号]
+   * @return {[type]}       [description]
+   */
+  getOmit(issue){
+    let self=this;
+    return new Promise((resolve,reject)=>{
+      $.ajax({
+        url:'/get/omit',
+        data:{
+          issue:issue
+        },
+        dataType:'json',
+        success:function(res){
+          self.setOmit(res.data);
+          resolve.call(self,res)
+        },
+        error:function(err){
+          reject.call(err);
+        }
+      })
+    });
+  }
+  /**
+   * [getOpenCode 获取开奖号码]
+   * @param  {string} issue [期号]
+   * @return {[type]}       [description]
+   */
+  getOpenCode(issue){
+    let self=this;
+    return new Promise((resolve,reject)=>{
+      $.ajax({
+        url:'/get/opencode',
+        data:{
+          issue:issue
+        },
+        dataType:'json',
+        success:function(res){
+          self.setOpenCode(res.data);
+          resolve.call(self,res);
+        },
+        error:function(err){
+          reject.call(err);
+        }
+      })
+    });
+  }
 
-    /**
-     * 获取当前的状态
-     * @param {string} issue 期号
-     */
-    getState(issue) {
-        let self = this;
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: '/get/state',
-                data: {
-                    issue
-                },
-                success: function (res) {
-                    resolve.call(self, res);
-                },
-                error: function(err) {
-                    reject.call(err);
-                }
-            })
-        })
-    }
-
+  /**
+   * [getState 获取当前状态]
+   * @param  {string} issue [当前期号]
+   * @return {[type]}       [description]
+   */
+  getState(issue){
+    let self=this;
+    return new Promise((resolve,rejet)=>{
+      $.ajax({
+        url:'/get/state',
+        data:{
+          issue:issue
+        },
+        dataType:'json',
+        success:function(res){
+          resolve.call(self,res);
+        },
+        error:function(err){
+          reject.call(err);
+        }
+      })
+    });
+  }
 }
 
-export default Interfaces;
+export default Interfaces
--- app/js/lottery/Timer.js
+++ app/js/lottery/Timer.js
@@ -1,47 +1,40 @@
-class Timer {
-    /**
-     * 
-     * @param {Number} end 截至时间
-     * @param {Function} update 更新的回调
-     * @param {Function} handle 倒计时结束的回调
-     * 
-     */
-    countDown(end, update, handle) {
-        const now = new Date().getTime();
-        const self = this;
-        if (now - end) {
-            handle.call(self);
-        } else {
-            let lastTime = end - now;
-            const pxD = 1000 * 60 * 60 * 24; //一天有多少毫秒
-            const pxH = 1000 * 60 * 60; // 一小时有多少毫秒
-            const pxM = 1000 * 60; // 一分钟有多少毫秒
-            const pxS = 1000; //一秒有多少毫秒
-            let d = Math.floor(lastTime / pxD);
-            let h = Math.floor((lastTime - d * pxD) / pxH);
-            let m = Math.floor((lastTime - d * pxD - h * pxH) / pxM);
-            let s = Math.floor((lastTime - d * pxD - h * pxH - m * pxM) / pxS);
-
-            let r = [];
-            if (d > 0) {
-                r.push(`<em>${d}</em>天`)
-            }
-            if (r.length || (h > 0)) {
-                r.push(`<em>${h}</em>时`)
-            }
-            if (r.length || m > 0) {
-                r.push(`<em>${m}</em>分`)
-            }
-            if (r.length || s > 0) {
-                r.push(`<em>${s}</em>秒`)
-            }
-            self.lastTime = r.join('');
-            update.call(self, r.join(''));
-            setTimeout(function () {
-                self.countDown(end, update, handle);
-            }, 1000);
+class Timer{
+    countdown(end,update,handle){
+      const now=new Date().getTime();
+      const self=this;
+      if(now-end>0){
+        handle.call(self);
+      }else{
+        let last_time=end-now;
+        const px_d=1000*60*60*24;
+        const px_h=1000*60*60;
+        const px_m=1000*60;
+        const px_s=1000;
+        let d=Math.floor(last_time/px_d);
+        let h=Math.floor((last_time-d*px_d)/px_h);
+        let m=Math.floor((last_time-d*px_d-h*px_h)/px_m);
+        let s=Math.floor((last_time-d*px_d-h*px_h-m*px_m)/px_s);
+        let r=[];
+        if(d>0){
+          r.push(`<em>${d}</em>天`);
         }
+        if(r.length||(h>0)){
+          r.push(`<em>${h}</em>时`);
+        }
+        if(r.length||m>0){
+          r.push(`<em>${m}</em>分`);
+        }
+        if(r.length||s>0){
+          r.push(`<em>${s}</em>秒`);
+        }
+        self.last_time=r.join('');
+        update.call(self,r.join(''));
+        setTimeout(function () {
+          self.countdown(end,update,handle);
+        }, 1000);
+      }
     }
-}
-
-export default Timer;
+  }
+  
+  export default Timer
+  
